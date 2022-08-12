760535877cf718869cf8c4ed078e1ff23029e733
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-13 00:42:24 +0100

    imports the footer component to the Homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { Appointment } from "../appointment/appointment";
 import { Dosage } from "../dosage/Dosage";
+import { Footer } from "../footer/footer";
 import { Vitals } from "../vitals/vitals";
+
 import "./Homepage.css";
 
 export const Homepage = () => {
@@ -32,6 +34,7 @@ export const Homepage = () => {
           </div>
         </div>
       </section>
+      <Footer />
     </>
   );
 };
