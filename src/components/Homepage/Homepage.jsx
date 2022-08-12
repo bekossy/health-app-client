daa5ffd0d13c9089acb6f8b6ff698298aebef188
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-12 15:55:22 +0100

    adds the vitals section in Homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -18,6 +18,16 @@ export const Homepage = () => {
             />
           </svg>
         </form>
+
+        <div className="vitals">
+          <h1>your Vitals</h1>
+          <div className="vitalContainer">
+            <h3>Heart rate: 21bpm</h3>
+            <h3>Blood pressure: 120/80mmhg</h3>
+            <h3>Heart rate: 21bpm</h3>
+            <h3>Blood pressure: 120/80mmhg</h3>
+          </div>
+        </div>
       </section>
     </>
   );
