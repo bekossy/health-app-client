06fe485417002c9a6cd8066c3d9e11fe58c701ce
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-13 00:12:19 +0100

    folder for the vitals section

---
+++ src/components/vitals/vitals.jsx
@@ -0,0 +1,15 @@
+export const Vitals = () => {
+  return (
+    <>
+      <div className="vitals">
+        <h1>your Vitals</h1>
+        <div className="vitalContainer">
+          <h3>Heart rate: 21bpm</h3>
+          <h3>Blood pressure: 120/80mmhg</h3>
+          <h3>Glucose level: 130 mg/dLm</h3>
+          <h3>Weight: 75kg</h3>
+        </div>
+      </div>
+    </>
+  );
+};
