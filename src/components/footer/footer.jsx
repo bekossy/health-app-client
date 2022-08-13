f40ce38379795a877bc60df3d47a5c9aaed988e6
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-13 01:35:48 +0100

    adds feedback input in the footer section

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <form>
+          <input type="text" name="feedback" id="feedback" />
+          <button type="submit">FEEDBACK</button>
+        </form>
+      </footer>
+    </>
+  );
+};
