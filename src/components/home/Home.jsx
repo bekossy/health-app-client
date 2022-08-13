f30172d3cf961b5b6413e2a9ec262c52101a1e10
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-13 01:36:35 +0100

    imports footer component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import Settings from "../settings/Settings";
 import { Homepage } from "../Homepage/Homepage";
 import "./Home.css";
+import { Footer } from "../footer/footer";
 
 const Home = () => {
   const [settings, setSettings] = useState(true);
-  return <>{settings ? <Homepage /> : <Settings />}</>;
+  return (
+    <>
+      {settings ? <Homepage /> : <Settings />}
+      <Footer />
+    </>
+  );
 };
 export default Home;
